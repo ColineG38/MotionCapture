29d02bfcb7705a1d9bd5b6fe370acd38cbd27bc8
ColineG38 <127790079+ColineG38@users.noreply.github.com>
2024-05-17 15:15:11 +0200

    Update Precision_big.py
    avec calcul des moyennes et écart type

--- code/Precision_big.py
+++ code/Precision_big.py
@@ -13,12 +13,26 @@ import glob
 
 os.chdir('C:/Users/colin/Documents/L3 ENS/Stage/Précision')
 
+#créer des dataframes pour stocker les moyennes et les écarts types des distances
+lignes = []
+for i in range(1,7):
+    for j in range(1,i):
+      lignes.append('d'+str(i)+str(j))
+
+colonnes = []    
+for file in glob.glob('*.csv'):
+    colonnes.append(file[:-4])
+
+moy = pd.DataFrame(0, index=lignes, columns=colonnes)    
+std = pd.DataFrame(0, index=lignes, columns=colonnes) 
+
+
 for file in glob.glob('*.csv'):
     
     df = pd.read_csv(file,sep=',',skiprows=2,header=None)
     
     if df.shape[1]!=45:
-        print(file,': problème')
+        print(file[:-4],': problème')
         
     else:
         #MISE EN FORME DU DATAFRAME
@@ -43,27 +57,34 @@ for file in glob.glob('*.csv'):
         df = df.drop([df.index[0],df.index[1]],axis=0) #supprimer les lignes dont on a plus besoin
 
         df = df.reset_index(drop = True) #réindexer les lignes à partir de 0
+        
 
         #EXPLOITATION DES DONNEES
 
         time = pd.to_numeric(df['Time'])
-
-        #tracer les distances entre toutes les combinaisons de marqueurs
-        k=0
+        
+        k=0 #indice pour le subplot
         plt.figure(figsize=(18,15))
         for i in range(1,7):
-            for j in range(1,i):
-                k+=1
+            for j in range(1,i): #explorer toutes les paires de marqueurs
+            
+                #calcul de la distance entre les marqueurs
+                k+=1 #indice pour le subplot
                 plt.subplot(5,3,k)
                 Mi = 'Marker'+str(i)
                 Mj = 'Marker'+str(j)
                 d = np.sqrt((pd.to_numeric(df[Mi+'X'])-pd.to_numeric(df[Mj+'X']))**2+(pd.to_numeric(df[Mi+'Y'])-pd.to_numeric(df[Mj+'Y']))**2+(pd.to_numeric(df[Mi+'Z'])-pd.to_numeric(df[Mj+'Z']))**2)
-
+                
+                #affichage des distances
                 plt.plot(time,d)
                 plt.xlabel('t')
                 plt.ylabel('d'+str(i)+str(j))
                 plt.title(str(file)[:-4]+' d'+str(i)+str(j)+'(t)')
                 plt.gcf().subplots_adjust(wspace=0.3,hspace = 0.6)
+                
+                #calcul de la moyenne et de l'écart type
+                moy.loc['d'+str(i)+str(j),file[:-4]] = np.nanmean(d)
+                std.loc['d'+str(i)+str(j),file[:-4]] = np.nanstd(d)
+                
         plt.savefig(str(file)[:-4]+".png")
         plt.show()
-        
